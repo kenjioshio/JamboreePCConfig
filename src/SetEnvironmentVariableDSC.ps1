0edfd444027f0a29223bef5a6d0c5a43bc72d63b
kenji_oshio <kenji_oshio@ot.olympus.co.jp>
2017-12-27 10:04:38 +0900

    Configuraion name was wrong.So, it fixed.

--- src/SetEnvironmentVariableDSC.ps1
+++ src/SetEnvironmentVariableDSC.ps1
@@ -15,7 +15,7 @@ Configuration SetEnvironmentVariable
             Value = "%systemroot%\System32\WindowsPowerShell\v1.0\;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\ProgramData\chocolatey\bin;C:\Program Files\TortoiseSVN\bin;C:\Program Files\NUnit 2.6.4\bin;C:\Program Files\Git\cmd;C:\Program Files\Java\jdk1.8.0_152\bin;"
         }
 
-        Environment EnvironmentVariable_VS120COMNTOOLS
+        Environment EnvironmentVariable_Java_Home
         {
             Ensure = "Present"  # You can also set Ensure to "Absent"
             Name = "JAVA_HOME"          
